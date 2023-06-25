a5aea5adf9aebba71b50e2e940baa436f9605c43
Spookyy12 <131680995+Spookyy12@users.noreply.github.com>
2023-06-25 20:56:26 +0300

    first commit

---
+++ src/main/java/ru/netology/statistic/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.statistic;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    boolean subscription;
+    public FormDate birthday;
+}
